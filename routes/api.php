c8714b1984e8db79fdfdc057068351a9ffa92db6
mattrifaldi <mattripaldi@gmail.com>
2026-09-05 10:31:17 +0700

    Pertemuan 2 : Initialisasi api & migrasi

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+// disini http method get, menampikan hello world
+// dengan parameter name, by default adalah Ahmad
+Route::get('/hello-world', function() {
+    $name = request()->get('name') ?? 'Ahmad'; 
+
+    return response()->json([
+        'message' => "Hello World, {$name}"
+    ]);
+});
+
+// blogs : untuk menampikan semua data blogs (GET)
+// blogs : untuk menambahkan data blog baru (POST)
+// blog/{id} : untuk menampilkan data blog berdasarkan id (GET)
+// blog/{id} : untuk mengupdate data blog berdasarkan id (PUT)
+// blog/{id} : untuk menghapus data blog berdasarkan id (DELETE)
